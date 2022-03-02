b3f39a6a17bb76cda5bf445c92ef8b87b52bc549
Alicia Kmak <alicia.kmak@gmail.com>
2022-03-02 22:29:27 +0100

    basic crud routes working ok

--- routes/restaurants.routes.js
+++ routes/restaurants.routes.js
@@ -30,3 +30,47 @@ router.get('/restaurants', async (req, res, next) => {
 })
 
 module.exports = router;
+
+// show restaurant details
+
+router.get('/restaurants/:id', async (req, res, next) => {
+
+  const { id } = req.params;
+
+  try {
+    const restaurant = await Restaurant.findById(id);
+    if (restaurant === null) {
+      return res.status(404).json({error: 'Restaurant not found'})
+    }
+    return res.json(restaurant);
+  }
+  catch (error) {
+    next(error);
+  }
+})
+
+// update resstaurant
+
+router.put('/restaurants/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, neighborhood, cuisine, budget, ambience, priority, veganMenu, glutenFree, notes } = req.body;
+  
+  try {
+    const restaurant = await Restaurant.findByIdAndUpdate(id, { name, neighborhood, cuisine, budget, ambience, priority, veganMenu, glutenFree, notes });
+    res.json(restaurant);
+  } 
+  catch (error) {
+    next(error);
+  }  
+})
+
+// delete restaurant
+router.delete('/restaurants/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const restaurant = await Restaurant.findByIdAndDelete(id);
+    res.json(restaurant);
+  } catch (error) {
+    next(error);
+  }    
+})
